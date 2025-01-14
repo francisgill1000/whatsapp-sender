4c02e8096c35a16032a427d12768c211bf9a0d76
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2025-01-14 22:53:42 +0400

    test

--- frontend/middleware/userType.js
+++
@@ -1,7 +0,0 @@
-const data = async ({ $auth, redirect }) => {
-  const { user_type, role } = $auth.user;
-  redirect("/dashboard2");
-  return "";
-};
-
-export default data;
--- frontend/plugins/vue-signature-pad.js
+++
@@ -1,6 +0,0 @@
-// plugins/vue-signature-pad.js
-
-import Vue from 'vue'
-import VueSignaturePad from 'vue-signature-pad'
-
-Vue.use(VueSignaturePad)
